4dcbee8da117fecf5a7a9f4195d934aed6eef011
chris <git@pythoud.net>
2024-12-04 11:13:36 -0500

    Add tkiz-svg-demo-client module and initial setup
    Introduced a new module, tkiz-svg-demo-client, with a basic structure including an App.java. Configured the project's pom.xml to include this module and added necessary Maven dependencies and plugins to support build and execution processes.

---
+++ tkiz-svg-demo-client/src/main/java/org/beanmaker/converters/demo/tkiz2svg/client/App.java
@@ -0,0 +1,10 @@
+package org.beanmaker.converters.demo.tkiz2svg.client;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
