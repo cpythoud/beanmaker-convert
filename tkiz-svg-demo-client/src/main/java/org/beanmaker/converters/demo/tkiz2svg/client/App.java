0c90d4495c210e6c27620fde4278a6e200f6a73b
chris <git@pythoud.net>
2024-12-05 15:28:38 -0500

    Update BASE_URL and add startup script for demo
    Changed the BASE_URL in App.java to point to the new server at london3.texngine.org, and increased the thread sleep duration to 200ms to reduce polling frequency. Added a new startup script start.sh to streamline the launching process of the tkiz-svg-demo application and redirect output logs to a file.

--- tkiz-svg-demo-client/src/main/java/org/beanmaker/converters/demo/tkiz2svg/client/App.java
+++ tkiz-svg-demo-client/src/main/java/org/beanmaker/converters/demo/tkiz2svg/client/App.java
@@ -4,7 +4,8 @@ import org.beanmaker.converters.lib.client.ConversionJobCreator;
 
 public class App {
 
-    private static final String BASE_URL = "http://localhost:8080/convert/tkiz2svg";
+    //private static final String BASE_URL = "http://localhost:8080/convert/tkiz2svg";
+    private static final String BASE_URL = "http://london3.texngine.org:8080/convert/tkiz2svg";
     private static final String TEST_SOURCE_FILENAME = "/home/chris/Prov/tkiz2svg/test-client/source/radar.tex";
     private static final String TEST_DESTINATION_FILENAME = "/home/chris/Prov/tkiz2svg/test-client/result/radar.svg";
 
@@ -14,7 +15,7 @@ public class App {
         job.startConversion();
         while (!job.ready()) {
             try {
-                Thread.sleep(100);
+                Thread.sleep(200);
                 System.out.println("Waiting for conversion to finish...");
             } catch (InterruptedException e) {
                 e.printStackTrace();
---
+++ tkiz-svg-demo/src/main/bash/start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/usr/bin/java -jar /home/chris/Prov/tkiz2svg/tkiz-svg-demo.jar >> /home/chris/Prov/tkiz2svg/log.txt 2>&1 &
